504457af1387540e48c4e227755430bc3c0dea45
Jiuzhou <nova2357@gmail.com>
2016-09-28 08:37:09 -0400

    basic ADS

---
+++ src/Cell.java
@@ -0,0 +1,10 @@
+
+public class Cell {
+	boolean obstable;
+	boolean dirt;
+	
+	public Cell(boolean ob, boolean dirt){
+		this.obstable = ob;
+		this.dirt = dirt;
+	}
+}
--- src/RobotApp.java
+++ src/RobotApp.java
@@ -4,11 +4,16 @@ public class RobotApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I am a Robot");
-		// Test pushing
-		//more test echo
-		//11111
-		// new thread more
-		// ******
+		
+		
+	
+	//generator
+		
+	//search
+		
+	//print 
+		
+	
 	}
-
+	
 }
